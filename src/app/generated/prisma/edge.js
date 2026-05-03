678314871ab1031e538f35fbd30410ac51bf1747
Emmanuelnzayisenga <emmanuelnzayisenga01@gmail.com>
2026-05-03 21:10:18 +0200

    mnmnmnmn

--- src/app/generated/prisma/edge.js
+++ src/app/generated/prisma/edge.js
@@ -256,7 +256,7 @@ const config = {
     "isCustomOutput": true
   },
   "relativeEnvPaths": {
-    "rootEnvPath": "../../../../.env",
+    "rootEnvPath": null,
     "schemaEnvPath": "../../../../.env"
   },
   "relativePath": "../../../../prisma",
