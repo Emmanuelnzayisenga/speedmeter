b79e65db8ac5940acfae0910b11520c32b1f6888
Emmanuelnzayisenga <emmanuelnzayisenga01@gmail.com>
2026-05-03 20:05:44 +0200

    first commit

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
-/** @type {import('next').NextConfig} */
 const nextConfig = {
   serverExternalPackages: ['@prisma/client', 'bcrypt'],
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 }
 
 module.exports = nextConfig
